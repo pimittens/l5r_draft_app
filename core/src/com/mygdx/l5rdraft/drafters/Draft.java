4354a8b4e0733b42808b8362a5888f6d347fd871
pimittens <white.riley@gmail.com>
2019-11-22 23:08:14 -0800

    cards

--- core/src/com/mygdx/l5rdraft/drafters/Draft.java
+++ core/src/com/mygdx/l5rdraft/drafters/Draft.java
@@ -1,5 +1,6 @@
 package com.mygdx.l5rdraft.drafters;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.mygdx.l5rdraft.packs.Pack;
 
 import java.util.ArrayList;
@@ -13,28 +14,91 @@ public class Draft {
     private List<Pack> packs;
     private List<Queue<Pack>> playerQueues;
 
+    private boolean draftIsOver;
+    private int roundNumber;
+
     public Draft(List<Pack> packs, List<Drafter> drafters) {
+        draftIsOver = false;
+        roundNumber = 1;
         this.packs = packs;
         this.drafters = drafters;
         playerQueues = new ArrayList<>();
-        for (Drafter d : drafters) {
+        for (int i = 0; i < drafters.size(); i++) {
             playerQueues.add(new LinkedList<>());
         }
     }
 
-    private void getMorePacks() {
-        
+    public boolean draftIsOver() {
+        return draftIsOver;
+    }
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+    /**
+     * updates the state of the draft
+     * should be called each time before a pack is pushed or pulled
+     */
+    public void update() {
+        // check if the round is over
+        if (roundIsOver()) {
+            roundNumber += 1;
+            // try to get more packs. if none are left the draft is over
+            if (!getMorePacks()) {
+                draftIsOver = true;
+            }
+        }
+
+    }
+
+    /**
+     * checks if the round is over
+     * the round is over when no player has a checked out pack and all player queues are empty
+     *
+     * @return whether the round is over
+     */
+    public boolean roundIsOver() {
+        for (Queue<Pack> q : playerQueues) {
+            if (!q.isEmpty()) {
+                return false;
+            }
+        }
+        for (Drafter d : drafters) {
+            if (d.hasCheckedOutPack()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * adds more packs to the player queues. returns whether there were enough packs left
+     * if there aren't enough packs left for another round, the draft should end
+     *
+     * @return whether there were enough packs left for another round
+     */
+    private boolean getMorePacks() {
+        if (packs.size() < drafters.size()) {
+            return true;
+        }
+        for (Queue<Pack> q : playerQueues) {
+            q.add(packs.remove(MathUtils.random(packs.size() - 1)));
+        }
+        return true;
     }
 
     /**
      * pushes a pack to the next drafter
      *
-     * @param pack the pack
+     * @param pack        the pack
      * @param drafterName the drafter who just picked from the pack
      */
     public void pushPack(Pack pack, String drafterName) {
         for (int i = 0; i < drafters.size(); i++) {
             if (drafters.get(i).getName().equals(drafterName)) {
+                // todo: validate the pack maybe
+                drafters.get(i).checkInPack();
                 int nextPos = i + 1;
                 if (nextPos >= playerQueues.size()) {
                     nextPos = 0;
@@ -55,7 +119,9 @@ public class Draft {
     public Pack getNextPack(String drafterName) {
         for (int i = 0; i < drafters.size(); i++) {
             if (drafters.get(i).getName().equals(drafterName)) {
-                return playerQueues.get(i).poll();
+                Pack p = playerQueues.get(i).poll();
+                drafters.get(i).checkOutPack(p);
+                return p;
             }
         }
         return null;
--- core/src/com/mygdx/l5rdraft/drafters/Drafter.java
+++ core/src/com/mygdx/l5rdraft/drafters/Drafter.java
@@ -1,11 +1,13 @@
 package com.mygdx.l5rdraft.drafters;
 
+import com.mygdx.l5rdraft.packs.Pack;
 import com.mygdx.l5rdraft.packs.Pool;
 
 public class Drafter {
 
     private Pool pool;
     private String name;
+    private Pack checkedOutPack;
 
     public Drafter(String name) {
         pool = new Pool();
@@ -18,5 +20,17 @@ public class Drafter {
 
     public String getName() {
         return name;
+    }
+
+    public void checkOutPack(Pack p) {
+        checkedOutPack = p;
+    }
+
+    public void checkInPack() {
+        checkedOutPack = null;
+    }
+
+    public boolean hasCheckedOutPack() {
+        return checkedOutPack != null;
     }
 }
